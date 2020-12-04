e4976d3c9992f2fcad3d9d0f5167090bb2ed4c24
BiYuqi <biyuqiwan@163.com>
2020-12-04 17:19:12 +0800

    feat: test template done.

--- packages/cli/src/run.ts
+++ packages/cli/src/run.ts
@@ -2,11 +2,13 @@ import { RawOptions } from '@ifake/pkg-shared'
 import { pkgTemplate } from '@ifake/pkg-template'
 import { eslintTemplate } from '@ifake/pkg-eslint'
 import { packageTemplate } from '@ifake/pkg-package'
+import { testTemplate } from '@ifake/pkg-test'
 import { transferOptions } from './utils/transfer-options'
 
 export async function run(options: RawOptions) {
   const finalOptions = transferOptions(options)
   await pkgTemplate(finalOptions)
   await eslintTemplate(finalOptions)
+  await testTemplate(finalOptions)
   await packageTemplate(finalOptions)
 }
--- packages/pkg-test/src/index.ts
+++ packages/pkg-test/src/index.ts
@@ -1,1 +1,9 @@
-export async function testTemplate() {}
+import { FinalOptions, fsExtra, pathResolve } from '@ifake/pkg-shared'
+
+export async function testTemplate(options: FinalOptions) {
+  const { appName, platform } = options
+  await fsExtra.copySync(
+    pathResolve(__dirname, `../template/${platform}`),
+    pathResolve(process.cwd(), appName)
+  )
+}
