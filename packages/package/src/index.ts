f0d203e746c4323cfc79cb77489626679165e0a5
BiYuqi <biyuqiwan@163.com>
2020-12-04 15:05:38 +0800

    chore: redefined login for generate package josn

--- packages/package/src/index.ts
+++ packages/package/src/index.ts
@@ -3,10 +3,10 @@ import handlebars from 'handlebars'
 import { FinalOptions, resolve } from '@ifake/pkg-shared'
 
 export function packageTemplate(options: FinalOptions) {
-  const { pkgName, platform, appName } = options
+  const { platform, appName } = options
   const pkgFile = fs
     .readFileSync(resolve(__dirname, `../template/${platform}/package.json.hms`))
     .toString('utf-8')
-  const result = handlebars.compile(pkgFile)({ pkgName })
+  const result = handlebars.compile(pkgFile)(options)
   fs.writeFileSync(resolve(process.cwd(), `./${appName}/package.json`), result)
 }
